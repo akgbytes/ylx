0875f8f49b77a66faff092dc3d7630d49d83c458
Aman Kumar Gupta <akgbytes@gmail.com>
2026-08-16 18:46:52 +0530

    feat: add shared YLX UI package

---
+++ packages/ui/src/icons.ts
@@ -0,0 +1,7 @@
+/**
+ * Shared icon library for YLX interfaces. Icons are provided by Tabler Icons.
+ *
+ * @example
+ * import { IconSearch } from "@ylx/ui/icons";
+ */
+export * from "@tabler/icons-react";
